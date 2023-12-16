a0b807a8945594ff7b39124a3994732ed735b09e
Posie04 <150278935+Posie04@users.noreply.github.com>
2023-12-16 20:00:35 +0700

    Update day21-22_project.sql

--- day21-22_project.sql
+++ day21-22_project.sql
@@ -44,5 +44,40 @@ select * from a
 order by year_month asc
 
 --- Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính ( Từ 1/2019-4/2022)
+/* Tìm các khách hàng có trẻ tuổi nhất và lớn tuổi nhất theo từng giới tính ( Từ 1/2019-4/2022)
+Output: first_name, last_name, gender, age, tag (hiển thị youngest nếu trẻ tuổi nhất, oldest nếu lớn tuổi nhất)
+Hint: Sử dụng UNION các KH tuổi trẻ nhất với các KH tuổi già nhất 
+tìm các KH tuổi trẻ nhất và gán tag ‘youngest’  
+tìm các KH tuổi già nhất và gán tag ‘oldest’ 
+Insight là gì? (Trẻ nhất là bao nhiêu tuổi, số lượng bao nhiêu? Lớn nhất là bao nhiêu tuổi, số lượng bao nhiêu) 
+Note: Lưu output vào temp table rồi đếm số lượng tương ứng 
+*/
+with a as
+(
+select first_name,last_name, gender, age,FORMAT_TIMESTAMP('%Y-%m',created_at),
+case 
+  when age=min(age) over(partition by gender) then 'youngest' 
+  when age=max(age) over(partition by gender) then 'oldest' end as tag
+from bigquery-public-data.thelook_ecommerce.users
+where FORMAT_TIMESTAMP('%Y-%m',created_at) between '2019-01' and '2022-04'
+),
+output as
+( 
+select first_name,last_name, gender, age,tag,
+count(tag) over(partition by gender,tag) from a
+where tag is not null)
+
+select tag,age,gender,
+count(tag) from output
+group by tag,age,gender
+
+=> insight: nhiều người già sử dụng hơn người trẻ, lớn nhất là 70 tuổi và nhỏ nhất là 12 tuổi
+
+
+
+
+
+
+
 
 
