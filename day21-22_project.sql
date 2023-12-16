ebd2784d34ef393ab1fa9738e70b9c79ab72fbe5
Posie04 <150278935+Posie04@users.noreply.github.com>
2023-12-17 01:12:27 +0700

    Day21-22_project.sql

--- day21-22_project.sql
+++ day21-22_project.sql
@@ -1,3 +1,4 @@
+---------------------------Day 21
 ---Ad-hoc tasks
 ---- Thống kê tổng số lượng người mua và số lượng đơn hàng đã hoàn thành mỗi tháng ( Từ 1/2019-4/2022)
 with a /*số người mua*/ as
@@ -104,6 +105,91 @@ sum(sale) as revenue from a
 where time between '2022-04-15' and '2022-07-15' 
 group by time,product_category
 order by time 
+
+---------------------------Day 22
+---tạo metric trước khi dựng dashboard
+/* Giả sử team của bạn đang cần dựng dashboard và có yêu cầu xử lý dữ liệu trước khi kết nối với BI tool. 
+Sau khi bàn bạc, team của bạn quyết định các metric cần thiết cho dashboard 
+và cần phải trích xuất dữ liệu từ database để ra được 1 dataset như mô tả 
+Hãy sử dụng câu lệnh SQL để tạo ra 1 dataset như mong muốn và lưu dataset đó vào VIEW đặt tên là vw_ecommerce_analyst */
+
+-- select * from bigquery-public-data.thelook_ecommerce.orders (month,year)
+
+select FORMAT_TIMESTAMP('%Y-%m', created_at) as month,
+extract (year from created_at) 
+from bigquery-public-data.thelook_ecommerce.orders
+
+-- select * from bigquery-public-data.thelook_ecommerce.products (product)
+
+select category from bigquery-public-data.thelook_ecommerce.products
+
+--TPV/TPO 
+select FORMAT_TIMESTAMP('%Y-%m', created_at) as month,
+sum (sale_price) as TPV,
+count(order_id) as TPO
+from bigquery-public-data.thelook_ecommerce.order_items
+group by FORMAT_TIMESTAMP('%Y-%m', created_at)
+
+
+--Total_cost
+with a as
+(
+select a.id,b.product_id,a.cost,
+b.created_at,b.sold_at
+from bigquery-public-data.thelook_ecommerce.products as a
+inner join bigquery-public-data.thelook_ecommerce.inventory_items as b
+on a.id=b.product_id
+where b.sold_at is null)
+select month, tongchiphi from
+( select 
+FORMAT_TIMESTAMP('%Y-%m', created_at) as month, sum(cost) as tongchiphi from a
+group by FORMAT_TIMESTAMP('%Y-%m', created_at)
+)
+order by month
+
+----Revenue_growth
+with a as
+(
+select month,TPV,
+lead(month) over(order by month asc) as next_month,
+lead(TPV) over(order by month asc) as next_TPV,
+from (
+select FORMAT_TIMESTAMP('%Y-%m', created_at) as month,
+sum (sale_price) as TPV,
+count(order_id) as TPO
+from bigquery-public-data.thelook_ecommerce.order_items
+group by FORMAT_TIMESTAMP('%Y-%m', created_at))
+order by month
+)
+select round(((next_TPV-TPV)*100/TPV),2) as revenue_growth
+from a
+
+---Order_growth
+with a as
+(
+select month,TPO,
+lead(month) over(order by month asc) as next_month,
+lead(TPO) over(order by month asc) as next_TPO,
+from (
+select FORMAT_TIMESTAMP('%Y-%m', created_at) as month,
+sum (sale_price) as TPV,
+count(order_id) as TPO
+from bigquery-public-data.thelook_ecommerce.order_items
+group by FORMAT_TIMESTAMP('%Y-%m', created_at))
+order by month
+)
+select round(((next_TPO-TPO)*100/TPO),2) as order_growth
+from a
+
+---Total_profit
+
+
+---Profit_to_cost_ratio
+
+
+
+
+
 
 
 
